36bb1166ea96d2286ef4c09e7b79b25f468297b7
aleffythiago <aleffythiago347@gmail.com>
2024-10-17 07:57:56 -0300

    Interface

--- Assets/Script/BolinhaMove.cs
+++ Assets/Script/BolinhaMove.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BolinhaMove : MonoBehaviour
 {
@@ -18,18 +21,26 @@ public class BolinhaMove : MonoBehaviour
      [SerializeField] private AudioClip pegaCubo; 
      private AudioSource audioPlayer;
      private bool estarVivo = true;
+     private TextMeshProUGUI textoPontos;
+     private TextMeshProUGUI textoTotal;
 
+    [Header("Emojis")]
+    [SerializeField] private List<Sprite> emojis = new List<Sprite>();
 
 
     // Start is called before the first frame update
     void Start()
     
     {
-        
+
 
         rb = GetComponent<Rigidbody>();
         audioPlayer = GetComponent<AudioSource>();
         estarVivo = true;
+        textoPontos = GameObject.Find("pontos").GetComponent<TextMeshProUGUI>();
+        textoTotal = GameObject.Find("totalPontos").GetComponent<TextMeshProUGUI>();
+        textoTotal.text = GameObject.FindGameObjectsWithTag("CuboBrilhante").Length.ToString();
+        
 
     }
 
@@ -47,6 +58,8 @@ public class BolinhaMove : MonoBehaviour
             rb.AddForce(transform.up* forcaPulo, ForceMode.Impulse);
             audioPlayer.PlayOneShot(pulo);
         }
+
+        VerificaObjetivo();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -55,6 +68,7 @@ public class BolinhaMove : MonoBehaviour
             Destroy(other.gameObject);
             audioPlayer.PlayOneShot(pegaCubo);
             pontos++;
+            textoPontos.text = pontos.ToString();
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -64,5 +78,39 @@ public class BolinhaMove : MonoBehaviour
             estarVivo = false;
         }
     }
+    private void VerificaObjetivo()
+    {
+        int totalCubos = Int32.Parse(textoTotal.text);
+        TextMeshProUGUI objetivo = GameObject.Find("Objetivo").GetComponent<TextMeshProUGUI>();
+
+        Image emoji = GameObject.Find("Emoji").GetComponent<Image>();
+
+        Debug.LogFormat($"Pontos: {pontos},Total cubos: {totalCubos}");
+        if(pontos < totalCubos)
+        {
+           
+            objetivo.text = "pegue todos os cubos";
+            emoji.sprite = emojis[0];
+        }
+         if(pontos >= totalCubos / 2)
+        {
+            
+            objetivo.text = "continue assim,você ja pegou a metade";
+            emoji.sprite = emojis[1];
+        }
+        if(pontos >= totalCubos -5)
+        {
+           
+            objetivo.text ="quase no fim";
+            emoji.sprite = emojis[2];
+
+        }
+        if(pontos == totalCubos)
+        {
+            
+            objetivo.text = "todos os cubos coletados,passagem liberada";
+            emoji.sprite = emojis[3];
+        }
+    } 
 }
         
